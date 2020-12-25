6652b0c47ee339ec5e5985b8d9a9c443a7b50a3c
Ayush Jain <ayush.ayushjain12@gmail.com>
2020-12-25 11:10:06 +0530

    added google Oauth

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-const express = require('express');
+const express           = require('express');
+const passport          = require('passport');
+const GoogleStrategy    = require('passport-google-oauth20').Strategy;  
+
+const keys = require('./config/secretKeys')
 
 const app = express();
 
-app.get('/', (request, response) => {
-    response.send({'first': 'Ayush Jain'});
-})
+passport.use(new GoogleStrategy(
+        {   clientID     : keys.google_client_id,
+            clientSecret : keys.google_client_secret,
+            callbackURL  : '/auth/google/callback'
+        }, (accessToken, refreshToken, profile, cb) => {
+
+            console.log('accessToken',  accessToken);
+            console.log('refreshToken', refreshToken);
+            console.log('profile',      profile);
+        } 
+    ));
+
+app.get('/auth/google', passport.authenticate('google' ,{scope: ['email', 'profile']}))
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT  = process.env.PORT || 5000 ;
 app.listen(PORT, ()=> console.log('server is created on port', PORT))
