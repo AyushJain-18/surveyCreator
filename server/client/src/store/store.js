ec5f935cc099568efc74c2dd064989df18497dfe
Ayush Jain <ayush.ayushjain12@gmail.com>
2021-01-09 18:22:18 +0530

    added logger for production

--- server/client/src/store/store.js
+++ server/client/src/store/store.js
@@ -8,9 +8,9 @@ import authReducers from './authReducers/auth.reducers'
 const middleware = [];
 middleware.push(thunk);
 
-if(process.env.NODE_ENV ==="development"){
+//if(process.env.NODE_ENV ==="development"){
     middleware.push(logger);
-}
+// }
 // reducers 
 const reducers = combineReducers({
     auth: authReducers
